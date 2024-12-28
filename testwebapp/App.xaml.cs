674479b358589c1af9cf81e168cc62ef696869ea
Syed Amaan Hasan <SyedAmaanHasan@DESKTOP-9BGS792>
2024-12-28 19:33:35 +0530

    Add project files.

---
+++ testwebapp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace testwebapp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
